97ab48f262bf8b9c564114906b93b1253f1565e4
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-21 17:28:42 -0600

    feat: allow connection with client (react)

--- Backend/server.js
+++ Backend/server.js
@@ -8,10 +8,18 @@ const app = express();
 
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
 app.use(logger('dev'));
