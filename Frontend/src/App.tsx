898eae5c4f91a31d2f47152274c3dfbf5967612f
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-21 17:57:01 -0600

    feat: show server message

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,23 +1,53 @@
-import './App.css'
-import { io } from 'socket.io-client'
+import { useEffect } from 'react';
+import './App.css';
+import { io } from 'socket.io-client';
 
 function App() {
-
+  // Create the socket connection outside useEffect to avoid creating multiple connections
   const socket = io('http://localhost:3000');
+
+  useEffect(() => {
+    // Handle incoming messages from the server
+    const handleServerMessage = (message: string) => {
+      const ul = document.getElementById('messages') as HTMLUListElement;
+      const li = document.createElement('li');
+      li.textContent = message;
+      li.classList.add('text-red-500');
+      ul.appendChild(li);
+    };
+
+    socket.on('server-message', handleServerMessage);
+
+    // Cleanup the effect to avoid memory leaks
+    return () => {
+      socket.off('server-message', handleServerMessage);
+    };
+  }, [socket]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const message = (document.getElementById('message') as HTMLInputElement).value;
     if (message) {
-      socket.emit('message', message);
+      socket.emit('client-message', message);
+      const ul = document.getElementById('messages') as HTMLUListElement;
+      const li = document.createElement('li');
+      li.textContent = message;
+      li.classList.add('text-blue-500');
+      ul.appendChild(li);
       (document.getElementById('message') as HTMLInputElement).value = '';
       console.log('Message sent:', message);
     }
-  }
+  };
 
   return (
     <>
       <div className='m-auto w-96 h-dvh border-2 rounded-2xl border-white flex flex-col'>
+        <div className='flex-1 p-2 border-b-2 border-white'>
+          <h2 className='text-white'>Welcome to the chat!</h2>
+        </div>
+        <ul id='messages' className='h-full overflow-y-auto'>
+          {/* Messages will be appended here */}
+        </ul>
         <form onSubmit={handleSubmit} className='flex flex-col justify-between h-full'>
           <div className='flex justify-between mt-auto'>
             <input type="text" name='message' id='message' placeholder='Type a message' autoComplete='off' className='flex-1 mr-2 p-2 border border-gray-300 rounded'/>
@@ -26,7 +56,7 @@ function App() {
         </form>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
