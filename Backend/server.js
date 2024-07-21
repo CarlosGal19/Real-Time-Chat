621e6ed55a79ba95dda8d2b6c860d579541d459d
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-21 16:59:14 -0600

    feat: create socket.io server

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import logger from 'morgan';
 
+import { Server } from 'socket.io';
+import { createServer } from 'node:http'
+
 const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -10,6 +21,6 @@ app.get('/', (req, res) => {
     res.send({msg: 'Hello World'});
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
