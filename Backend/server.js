11746bf0d64a80a7feadfb4584f264dd3639bfd4
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-21 17:56:36 -0600

    feat: recover messages if there no connection

--- Backend/server.js
+++ Backend/server.js
@@ -12,13 +12,18 @@ const io = new Server(server, {
     cors: {
         origin: 'http://localhost:5173',
         methods: ['GET', 'POST']
-    }
+    },
+    connectionStateRecovery: {}
 });
 
 io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('disconnect', () => {
         console.log('A user disconnected');
+    });
+    socket.on('client-message', (msg) => {
+        console.log('Client message: ' + msg);
+        io.emit('server-message', 'Server response');
     });
 });
 
